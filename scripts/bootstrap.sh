1ccc2f6520416ec8f1cd44bb95aacee8dec63b9a
Radu Micu <radu.micu@amazerealise.com>
2019-04-10 23:07:20 +0100

    WIP - New dotfiles structure

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+###############################################################################
+# Install                                                                     #
+###############################################################################
+
+echo "Setting up my Mac …"
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.install` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+# Dependencies                                                                #
+###############################################################################
+
+# Install all the things
+source ~/.dotfiles/homebrew/install.sh
+source ~/.dotfiles/node/install.sh
+source ~/.dotfiles/nvm/install.sh
+source ~/.dotfiles/rust/install.sh
+source ~/.dotfiles/oh-my-zsh/install.sh
+
+###############################################################################
+# Symlinks                                                                    #
+###############################################################################
+
+source ~/.dotfiles/system/.symlinks
+
+###############################################################################
+# macOS                                                                    #
+###############################################################################
+
+# Set macOS preferences
+# Run last because this will reload the shell
+source ~/.dotfiles/macos/.defaults
