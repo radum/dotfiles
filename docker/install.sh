24a1a8775d9e1649182ea91ec2961f3e46e6232b
Radu Micu <radu.micu@gmail.com>
2020-04-19 14:24:29 +0100

    Fix fzf completion, add docker completion files also
    Fix fzf completion, add docker completion files also

---
+++ docker/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+print_sub_heading "Docker"
+
+print_task "Add Docker ZSH completion files to 'HOME/.docker/completions'"
+
+mkdir -p "$HOME/.docker/completions"
+
+if command -v docker-compose >/dev/null 2>&1; then
+	curl -sL https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose \
+		-o "$HOME/.docker/completions/_docker-compose"
+fi
+if command -v docker-machine >/dev/null 2>&1; then
+	curl -sL https://raw.githubusercontent.com/docker/machine/master/contrib/completion/zsh/_docker-machine \
+		-o "$HOME/.docker/completions/_docker-machine"
+fi
+if command -v docker >/dev/null 2>&1; then
+	curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
+		-o "$HOME/.docker/completions/_docker"
+fi
--- fzf/completion.zsh
+++ fzf/completion.zsh
@@ -1,7 +1,16 @@
 #!/bin/bash
-test -d /usr/local/opt/fzf/shell || return 0
+
+# Fuzzy completion for bash and zsh
+# https://github.com/junegunn/fzf#fuzzy-completion-for-bash-and-zsh
+#
+#  Files under current directory
+#  - You can select multiple items with TAB key
+#  vim **<TAB>
+#  Directories under current directory (single-selection)
+#  cd **<TAB>
+test -d $(brew --prefix)/opt/fzf/shell || return 0
 
 export FZF_COMPLETION_TRIGGER='**'
 
 # shellcheck disable=SC1091,SC1094
-. /usr/local/opt/fzf/shell/completion.zsh
+. $(brew --prefix)/opt/fzf/shell/completion.zsh
