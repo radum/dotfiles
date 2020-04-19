058671ee190e0de2c3af505ecb85874588b9dc68
Radu Micu <radu.micu@gmail.com>
2020-04-19 14:31:03 +0100

    Add more info to docker completion installs

--- docker/install.sh
+++ docker/install.sh
@@ -9,12 +9,18 @@ mkdir -p "$HOME/.docker/completions"
 if command -v docker-compose >/dev/null 2>&1; then
 	curl -sL https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose \
 		-o "$HOME/.docker/completions/_docker-compose"
+
+	print_result $? "docker-compose completion installed"
 fi
 if command -v docker-machine >/dev/null 2>&1; then
 	curl -sL https://raw.githubusercontent.com/docker/machine/master/contrib/completion/zsh/_docker-machine \
 		-o "$HOME/.docker/completions/_docker-machine"
+
+	print_result $? "docker-machine completion installed"
 fi
 if command -v docker >/dev/null 2>&1; then
 	curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
 		-o "$HOME/.docker/completions/_docker"
+
+	print_result $? "docker completion installed"
 fi
