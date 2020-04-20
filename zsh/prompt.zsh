41b3933db962aa7c6900e887fff0e64f0eee4b87
Radu Micu <radu.micu@gmail.com>
2020-04-20 09:24:51 +0100

    Remove VI mode from spaceshop prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# https://github.com/denysdovhan/spaceship-prompt/blob/master/docs/Troubleshooting.md#what-is-the-i-before-prompt-character-
+export SPACESHIP_VI_MODE_SHOW=false
 export SPACESHIP_CHAR_SUFFIX=" "
 export SPACESHIP_CHAR_SYMBOL=»
 # SPACESHIP_CHAR_SYMBOL=➜
