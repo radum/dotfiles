8bb35bd4f9a089dd364d11a3fcae017707002b0e
radum <radu.micu@gmail.com>
2024-03-05 22:47:44 +0000

    Update prezto setup

--- prezto/install.sh
+++ prezto/install.sh
@@ -12,3 +12,8 @@ print_sub_heading "Prezto"
 
 # TODO: Add check if its installed and update if it is
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# prezto-contrib
+# https://github.com/belak/prezto-contrib
+cd $ZPREZTODIR
+git clone --recurse-submodules https://github.com/belak/prezto-contrib contrib
